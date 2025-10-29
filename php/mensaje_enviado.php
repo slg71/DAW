2dd4349f66dbcd5ddb7c3407683c4582f2715032
slg71 <slg71@gcloud.ua.es>
2025-10-29 10:57:31 +0100

    mensaje enviado modificado con php

--- php/mensaje_enviado.php
+++ php/mensaje_enviado.php
@@ -1,3 +1,40 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //pillo los valores
+    $tipo_mensaje = trim($_POST["tipo_mensaje"] ?? "");
+    $mensaje = trim($_POST["mensaje"] ?? "");
+
+    // Tipos válidos
+    $tipos_validos = ["informacion", "cita", "oferta"];
+
+    // Validar
+    if (!in_array($tipo_mensaje, $tipos_validos) || $mensaje == "") {
+        // Redirigir de vuelta con error
+        header("Location: ../mensaje.html?error=empty");
+        exit;
+    }
+
+//plantilla
+// require_once "cabecera.php";
+// require_once "inicio.php";
+
+    echo "<h1>¡Mensaje Enviado con éxito!</h1>";
+    echo "<h2>Detalles del mensaje</h2>";
+    echo "<p>Tipo: " . htmlspecialchars($tipo_mensaje) . "</p>";
+    echo "<p> Mensaje: " . htmlspecialchars($mensaje) . "</p>";
+
+    //plantilla
+// require_once "pie.php";
+    exit;
+}else {
+    // Si se accede directamente al script sin enviar el formulario
+    header("Location: ../mensaje.html");
+    exit;
+}
+?>
+
+<!-- el html original:
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -42,4 +79,4 @@
         <p>© Leigh Garett & Maria Luisa Roca, Grupo 3 de Prácticas</p>
     </footer>
 </body>
-</html>
+</html> -->
