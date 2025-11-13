a1073ac2d667e82ed38079620c31be1bf5f813ba
slg71 <slg71@gcloud.ua.es>
2025-11-13 13:45:10 +0100

    correción de borrar todas las cookies al cerrar sesión

--- php/salir.php
+++ php/salir.php
@@ -1,21 +1,33 @@
 <?php
 session_start();
+
 $_SESSION = array();
 
-// borrar cookies y tmb las de la ult visita
-if (isset($_COOKIE['recordar_usuario'])) {
-    setcookie('recordar_usuario', '', time() - 3600, '/');
-    setcookie('recordar_pass', '', time() - 3600, '/');
-    setcookie('last_visit_recordar', '', time() - 3600, '/'); 
+// 2. Borrar la cookie que almacena el identificador de sesión
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 42000, '/');
 }
 
-//borrar las cookies de estilo
+// 3. Borrar las cookies de "recordar usuario"
+if (isset($_COOKIE['recordar_usuario'])) {
+    setcookie('recordar_usuario', '', time() - 3600, '/');
+}
+
+if (isset($_COOKIE['recordar_pass'])) {
+    setcookie('recordar_pass', '', time() - 3600, '/');
+}
+
+if (isset($_COOKIE['last_visit_recordar'])) {
+    setcookie('last_visit_recordar', '', time() - 3600, '/');
+}
+
+// 4. Borrar la cookie de estilo
 if (isset($_COOKIE['estilo'])) {
     setcookie('estilo', '', time() - 3600, '/');
 }
 
+// 5. Finalmente, destruir la sesión
 session_destroy();
-//rompre el array e igualar a cero
 
 header('Location: index.php');
 exit;
