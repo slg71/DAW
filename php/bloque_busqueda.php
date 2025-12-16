099e10ad2efae5755e64e2645cfa1aabf495d347
slg71 <slg71@gcloud.ua.es>
2025-12-16 21:38:16 +0100

    diagrama con datos reales + modular inicios

---
+++ php/bloque_busqueda.php
@@ -0,0 +1,10 @@
+<section id="busqueda">
+    <h2>Búsqueda rápida</h2>
+    <form action="resultado.php" method="get">
+        <fieldset>
+            <legend>Datos de búsqueda</legend>
+            <input type="text" id="buscar" name="buscar" placeholder="Ej: vivienda alquiler alicante">
+        </fieldset>
+        <button type="submit"><i class="icon-search"></i>Buscar</button>
+    </form>
+</section>
