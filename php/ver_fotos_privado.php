5e6f0956a72ba14eda822729da942f3faf0504ad
mlrv-git <mlrv4@mscloud.ua.es>
2025-12-16 19:05:07 +0100

    Paginacion de imagenes y miniatura en Ver Fotos

--- php/ver_fotos_privado.php
+++ php/ver_fotos_privado.php
@@ -6,22 +6,27 @@
 ob_start();
 
 require_once "sesion_control.php";
-
-include "funciones_anuncios.php";
-
+require_once "funciones_anuncios.php";
+require_once "funciones_imagenes.php"; // Necesario para generar_miniatura()
 // Obtenemos el ID del anuncio de la URL
 $id_anuncio = isset($_GET["id"]) ? intval($_GET["id"]) : 0;
 
-// Obtenemos los datos del anuncio
 $anuncio_data = obtener_detalle_y_fotos_anuncio($id_anuncio);
 
+// Obtenemos los datos del anuncio
 $titulo_pagina = "Mis Fotos del Anuncio";
 include "paginas_Estilo.php";
 include "header.php";
+?>
 
-// Mostramos la galería
-mostrar_galeria_fotos($anuncio_data, true); // true = vista privada
+<main>
+    <?php 
+    
+    mostrar_galeria_fotos_paginada($anuncio_data, true, 2); // true = vista privada
+    ?>
+</main>
 
+<?php 
 include "footer.php";
 ob_end_flush();
 ?>
